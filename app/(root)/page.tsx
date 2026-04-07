7f045993e8267761ff085fdd794e91b68842c954
Odo Maurice{alphaCodes} <92038525+odomaurice@users.noreply.github.com>
2026-04-07 10:34:18 +0100

    worked on the website homepage, about and subsequent pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,16 @@
 import { HomepageSlider } from "@/components/slider/HomepageSlider"
-import Features from "./components/Features"
 import Footer from "@/components/navigation/Footer"
 import QhubBanner from "./components/QhubBanner"
+import LegacySections from "./components/LegacySections"
+
 
 export default function Page() {
 	return (
 		<div>
 			<HomepageSlider />
 			<QhubBanner />
-			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-				<Features />
+			<div className=" mx-auto px-4 sm:px-6 lg:px-4">
+				<LegacySections />
 			</div>
 			<Footer />
 		</div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{ protocol: 'https', hostname: 'bond001.com' },
+			{ protocol: 'https', hostname: 'odl.esut.edu.ng' },
+			{ protocol: 'https', hostname: 'images.unsplash.com' },
+			{ protocol: 'https', hostname: 'github.com' },
+		],
+	},
+}
 
 export default nextConfig
