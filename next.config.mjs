685a56670cef6d851b35261c7f8d1e4719c5c6b8
Kingsintersect <kingsintersect@gmail.com>
2026-07-29 03:12:53 +0100

    Debugging: fixed the missing image domain for image url in next.config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,33 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   images: {
-      remotePatterns: [{
-            protocol: 'https',
-            hostname: 'bond001.com'
-         },
-         {
-            protocol: 'https',
-            hostname: 'qhub.portal.sandbox.qverselearning.org'
-         },
-         {
-            protocol: 'https',
-            hostname: 'qhub.api.sandbox.qverselearning.org'
-         },
-         {
-            protocol: 'https',
-            hostname: 'images.unsplash.com'
-         },
-         {
-            protocol: 'https',
-            hostname: 'github.com'
-         },
-      ],
-   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bond001.com",
+      },
+      {
+        protocol: "https",
+        hostname: "odl.esut.edu.ng",
+      },
+      {
+        protocol: "https",
+        hostname: "qhub.portal.sandbox.qverselearning.org",
+      },
+      {
+        protocol: "https",
+        hostname: "qhub.api.sandbox.qverselearning.org",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "github.com",
+      },
+    ],
+  },
 }
 
 export default nextConfig
